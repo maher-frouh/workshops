c068edd4eddc1cb7304207a3dc2833c7441a8ff9
MAHER <184831@student.talland.nl>
2025-09-25 13:36:47 +0200

    zzz

---
+++ template/includes/nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="news.php">News</a></li>
+    </ul>
+</nav>
